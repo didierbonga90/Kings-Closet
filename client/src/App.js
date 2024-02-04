4d56bfa462883e524d5d0c6f23f5f0a6780293d1
didier <didi2moscow@gmail.com>
2024-02-04 09:55:26 -0500

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home'
 
-const App = () => (
-    <div className="App">
-      <h1>King's Closet</h1>
-    </div>
-)
+const App = () => {
+  return(
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/login" element={<Login/>}/>
+      <Route exact path="/register" element={<Register/>}/>
+    </Routes>
+  )
+}
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () =>{
+    return (
+        <div>
+            <h1>King's Closet</h1>
+            
+            <p>HOME</p>
+        </div>
+    )
+}
+
+
+export default Home
